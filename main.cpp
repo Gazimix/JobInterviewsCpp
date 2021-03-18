59fcbef3324b62e8067d260f35bf25cdcbcba7c2
Gazimix <chendetails@gmail.com>
2021-03-18 20:24:47 +0200

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+
 #include <algorithm>
 
+
+void testSightSeeing();
+
+void testNumIslands();
+
+void testIntegralImage();
 
 using namespace std;
 
@@ -116,16 +123,112 @@ vector <vector <int>> getResult(vector <vector <int>> sums, int K)
 	return res;
 }
 
+
+
+
+
+
+using namespace std;
+
+int maxScoreSightseeingPair(std::vector<int>& values) {
+	int largest = 0;
+	int largest_idx = 0;
+	int second_largest = 0;
+	int second_largest_idx= 0;
+	int best_score = 0;
+	for (int i = 0; i < values.size(); ++i){
+
+		if (values[i] >= largest){
+			second_largest_idx = largest_idx;
+			second_largest = largest;
+			largest_idx = i;
+			largest = values[i];
+
+		}
+		else if (values[i] >= second_largest){
+			second_largest = values[i];
+			second_largest_idx = i;
+		}
+		// int max = (largest_idx > second_largest_idx) ? largest_idx : second_largest_idx;
+		// int min = (largest_idx > second_largest_idx) ? second_largest_idx : largest_idx;
+		if (best_score < largest + second_largest){
+			best_score = largest + second_largest;
+		}
+		--largest;
+		--second_largest;
+	}
+	return best_score;
+}
+
+
+
+void numIslandsHelper(vector<vector<char>>& grid, int i, int j){
+	if (j < grid[0].size() && i < grid.size() && i >= 0 && j >= 0)
+	{
+		if (grid[i][j] == '0')
+		{
+			return;
+		}
+		grid[i][j] = '0';
+		numIslandsHelper(grid, i, j + 1);
+		numIslandsHelper(grid, i + 1, j);
+		numIslandsHelper(grid, i - 1, j);
+		numIslandsHelper(grid, i, j - 1);
+	}
+}
+
+
+int numIslands(vector<vector<char>>& grid) {
+	int result = 0;
+	for (int i = 0; i < grid.size(); ++i){
+		for (int j = 0; j < grid[0].size(); ++j){
+			if (grid[i][j] != '0'){
+				numIslandsHelper(grid, i,j);
+				++result;
+			}
+		}
+	}
+	return result;
+}
+
+
+
+
 int main()
 {
-	vector <vector <int>> v = {{1,2,3,4,5,6,7,8,9,10},{7,9,8,7,6,5,4,34,32,5},{7,9,8,7,6,5,4,34,32,
-												  5},{1,2,3,4,5,6,7,8,9,10}};
+	// testIntegralImage();
+	// testSightSeeing();
+	// testNumIslands();
+}
+
+void testIntegralImage()
+{
+	vector <vector <int>> v = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {7, 9, 8, 7, 6, 5, 4, 34, 32, 5}, {7, 9, 8, 7, 6, 5, 4, 34, 32,
+														   5},{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
 	print_vector(v);
 	// auto res = inefficientVersion(v, 1);
 	auto sums = sumUp(v);
 	print_vector(sums);
 	v = getResult(sums, 2);
 	print_vector(v);
+}
+
+void testNumIslands()
+{
+	vector<char> c1 = {'1', '1', '1'};
+	vector<char> c2 = {'0','1','0'};
+	vector<char> c3 = {'1','1','1'};
+	vector<vector<char>>c;
+	c.push_back(c1);
+	c.push_back(c2);
+	c.push_back(c3);
+	cout << numIslands(c);
+}
+
+void testSightSeeing()
+{
+	vector <int> v2 = {8, 1, 5, 2, 6};
+	cout << maxScoreSightseeingPair(v2);
 }
 
 
